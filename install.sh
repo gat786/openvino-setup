dc6db620fc9f67a89d0a98612da538583df21000
ganesh tiwari <gat786@studentpartner.com>
2020-06-17 17:57:29 +0000

    added gitignore and 2019.3.334 download steps

---
+++ install.sh
@@ -0,0 +1,6 @@
+apt update
+apt upgrade
+apt install cmake cpio ffmpeg -y
+
+wget http://registrationcenter-download.intel.com/akdlm/irc_nas/15944/l_openvino_toolkit_p_2019.3.334.tgz
+tar -xvf l_openvino_toolkit_p_2019.3.334.tgz
